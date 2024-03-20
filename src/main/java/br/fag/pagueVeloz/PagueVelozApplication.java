2a32eb34f83410e35fcae5292cc70d579baf0dc3
ViniciusHFC <vinicius.henry.fc@gmail.com>
2024-03-20 19:24:58 -0300

    Upload Project

---
+++ src/main/java/br/fag/pagueVeloz/PagueVelozApplication.java
@@ -0,0 +1,13 @@
+package br.fag.pagueVeloz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagueVelozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagueVelozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fag/pagueVeloz/PagueVelozApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fag.pagueVeloz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagueVelozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
