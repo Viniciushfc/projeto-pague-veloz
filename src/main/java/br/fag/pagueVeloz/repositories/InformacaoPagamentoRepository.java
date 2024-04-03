3dc559e741bcbf5e137793faa893e921f421a825
viniciushfc <vinicius.henry.fc@gmail.com>
2024-04-03 20:06:56 -0300

    removido coisa inutilizadas

--- src/main/java/br/fag/pagueVeloz/repositories/InformacaoPagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.fag.pagueVeloz.repositories;
-
-import br.fag.pagueVeloz.entities.informacaoPagamento.InformacaoPagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InformacaoPagamentoRepository extends JpaRepository<InformacaoPagamento, Long> {
-}
