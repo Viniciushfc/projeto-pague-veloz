cd96d389790e29f713cec714c5984bb1df1d6fd5
viniciushfc <vinicius.henry.fc@gmail.com>
2024-04-10 21:10:51 -0300

    criação de repository FolhaPagamento

---
+++ src/main/java/br/fag/pagueVeloz/repositories/FolhaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package br.fag.pagueVeloz.repositories;
+
+import br.fag.pagueVeloz.entities.FolhaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolhaPagamentoRepository extends JpaRepository<FolhaPagamento, Long> {
+}
