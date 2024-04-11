f05ddd7bd8381051b3c432fd622b6b86d8835682
viniciushfc <vinicius.henry.fc@gmail.com>
2024-04-10 22:13:45 -0300

    Adicão da Anotação @Id.

--- src/main/java/br/fag/pagueVeloz/entities/FolhaPagamento.java
+++ src/main/java/br/fag/pagueVeloz/entities/FolhaPagamento.java
@@ -4,6 +4,7 @@ import br.fag.pagueVeloz.services.BeneficioService;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FolhaPagamento {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
